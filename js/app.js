ef386dde0e9442b293ce63b06f8bf509552c302a
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-16 15:20:35 +0100

    add: messages in chat

--- js/app.js
+++ js/app.js
@@ -2,13 +2,27 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    console.log(contacts);
+    // console.log(contacts[1].messages);
+
     return {
-      users: contacts,
+      contactList: contacts,
+      msgToSend: "",
+      date: "",
+      chat: contacts,
     };
   },
 
-  methods: {},
+  methods: {
+    // sendMsg: function () {
+    //   let chat = this.contactList[0].messages;
+    //   //   console.log(chat);
+    //   chat.push({
+    //     date: "",
+    //     message: this.msgToSend,
+    //     status: "sent",
+    //   });
+    // },
+  },
 });
 
 app.mount("#app");
