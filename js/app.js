26ad0d596811324374c397a0c195c34abb8be90f
giuliogallo94 <giuliogallo1994@gmail.com>
2023-11-16 16:57:40 +0100

    add: send Message

--- js/app.js
+++ js/app.js
@@ -3,30 +3,32 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     // console.log(contacts[0].messages[this.length - 1].date);
-
+    console.log(contacts[0].messages);
     return {
       contactList: contacts,
       msgToSend: "",
       date: "",
       chat: contacts,
-      indexUser: "0",
-      //   lastAcces: contacts[indexUser].messages,
+      indexChat: "0",
+      //   lastAcces: contacts[indexChat].messages,
     };
   },
 
   methods: {
     showActiveChat(index) {
-      this.indexUser = index;
+      this.indexChat = index;
     },
-    // sendMsg: function () {
-    //   let chat = this.contactList[0].messages;
-    //   //   console.log(chat);
-    //   chat.push({
-    //     date: "",
-    //     message: this.msgToSend,
-    //     status: "sent",
-    //   });
-    // },
+    sendMsg: function () {
+      console.log("ciao");
+      let messagesList = this.contactList[this.indexChat].messages;
+      messagesList.push({
+        date: "",
+        message: this.msgToSend,
+        status: "sent",
+      });
+      this.msgToSend = "";
+      console.log(messagesList);
+    },
   },
 });
 
